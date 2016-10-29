829f63138b5f56f437149a1798cd13e3d72e2baf
Joseph DeChicchis <josephdechicchis@gmail.com>
2016-10-29 11:09:59 -0400

    changed port to 5000

--- facebook_bot.js
+++ facebook_bot.js
@@ -1,5 +1,5 @@
-var page_token = ("EAAIJdgZBdh0wBAHdxOmbqppAA0DI7qNAp0BBbfPOz7mzQABkGyZBMle2ZBPgaaSx76lCsF0iE5qF72Iv5N3TVK44s3wVHTNhcOiKQlOaoZBp9P9mpmX1EnGcfzspQ5TZC5ZBZCPQ81ZBnzbQP9PoFbiEiHg5i6Jgbc0PtLRcjgeKkgZDZD");
-var verify_token = ("kameron");
+var page_token = ("EAAS6MQj6ImsBAFqdLD5JjxbS7VDgH3rz6tUKApMXLWQiWIflDn1xWDmbnw7m9clXYWaG5UAs1u1cLYDubBwf4T3Tzr0KExy7HhyE546LSGSlcm4RQpCtZCI0HwgLGQ4UhtucC3EKZAVSqk76JRiWV6AIwkDGKTqWfHrcyWeAZDZD");
+var verify_token = ("joseph");
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
@@ -21,14 +21,14 @@ controller.hears(['hello', 'hi'], 'message_received', function(bot, message) {
     bot.reply(message, 'Hello.');
 });
 
-// If you wanted your bot to respond to additional "hears" such as Go Heels 
+// If you wanted your bot to respond to additional "hears" such as Go Heels
 // you could do that below using the same syntax from above.
 
 
 //added to stop the debug tick remarks in console
 controller.on('tick', function(bot, event) {});
 
-controller.setupWebserver(process.env.port || 7000, function(err, webserver) {
+controller.setupWebserver(process.env.port || 5000, function(err, webserver) {
     controller.createWebhookEndpoints(webserver, bot, function() {
         console.log('ONLINE!');
 

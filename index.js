2b5d7022eeebf0401e2fd3ae4534dde2d214348c
Joseph DeChicchis <josephdechicchis@gmail.com>
2016-10-29 12:09:18 -0400

    changed response text

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post('/webhook/', function (req, res) {
 				sendGenericMessage(sender)
 				continue
 			}
-			sendTextMessage(sender, "Sorry, I don't know what you meant by \"" + text.substring(0, 1000000) + ""\"")
+			sendTextMessage(sender, "Sorry, I don't know what you meant by \"" + text.substring(0, 200) + ""\"")
 		}
 		if (event.postback) {
 			let text = JSON.stringify(event.postback)

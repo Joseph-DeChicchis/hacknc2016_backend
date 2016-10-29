0ca3e4e29831ac462ffc493504cd40c126dd9934
Joseph DeChicchis <josephdechicchis@gmail.com>
2016-10-29 12:58:11 -0400

    removed token in funciton call

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.post('/webhook/', function (req, res) {
 		}
 		if (event.postback) {
 			let text = JSON.stringify(event.postback)
-			send_message.text(sender, "Postback received: "+text.substring(0, 200), token)
+			send_message.text(sender, "Postback received: "+text.substring(0, 200))
 			continue
 		}
 	}
